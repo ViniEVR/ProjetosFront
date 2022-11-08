2a5377e873c5b93a143f40a4aa1f52444739e4a6
Vinícius <vinimice42@gmail.com>
2022-11-08 17:15:35 -0300

    [Finalizando] Finalizando projeto para apresentacao

--- CSS/Materialize/extern.js
+++ CSS/Materialize/extern.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
     $('.modal').modal();
 });
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+
+$(document).ready(function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+        'onOpenEnd': initCarouselModal
+    });
+
+    function initCarouselModal() {
+        var elems = document.querySelectorAll('.carousel');
+        var instances = M.Carousel.init(elems, {'fullWidth': true});
+        instances[0].set(2);
+    }
+});
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
